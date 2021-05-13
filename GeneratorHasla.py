1b0b14116c956f1ed7b9cec76a0c3bd599458ba5
Franz476 <48163310+Franz476@users.noreply.github.com>
2021-05-13 11:52:37 +0200

    V1.2
    Dodano obsługę plików csv, wyszukanie hasła, sprawdzanie typów. Jest pog

--- GeneratorHasla.py
+++ GeneratorHasla.py
@@ -1,42 +1,71 @@
+#Dodać zapis do csv, automatyczne wyszukiwanie i wykrywanie powtórzeń.
+
 import string
 import sys
 import secrets
 import os
+import csv
+import pandas as pd
 
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-sciezka = str(desktop) + '\\' + 'Hasła.txt'
-
-target = input ('Do czego potrzebujesz hasła? ')
-size = int(input('Wprowadź długość hasła: '))
-chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-print(chars)
+always_true_until_false = True
+desktop = str(os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop'))
+sciezka_txt = desktop + '\\' + 'Hasła.txt'
+sciezka_csv = desktop + '\\' + 'Hasła.csv'
+chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
 def sprawdz_typ(wartosc):
     try:
         val = int(wartosc)
-        print("Prawidłowo wprowadzona wartość. Wynosi ona: ", val)
-
     except ValueError:
-        print("Wprowadzono nieprawidłowy typ zmiennej.")
+        print("Nieprawidłowa wartość.")
         sys.exit(0)
 
-sprawdz_typ(size)
+def generowanie_hasla():
+    target = str.upper(input ('Do czego potrzebujesz hasła? '))
+    size = int(input('Wprowadź długość hasła: '))
+    sprawdz_typ(size)
+    if size <= 0:
+        print('Długość hasła musi być dłuższa od 0.')
+        sys.exit(0)
+    haslo = ''
+    for i in range(size):
+        k = secrets.choice(chars)
+        haslo += k
+    row = [target, haslo]       
+    file_csv = open(sciezka_csv, 'a')
+    file_csv_write = target + ', ' + haslo + '\n'
+    file_csv.write(file_csv_write)
 
-if size <= 0:
-    print('Długość hasła musi być dłuższa od 0.')
-    sys.exit(0)
-
-haslo = ''
-
-for i in range(size):
-    k = secrets.choice(chars)
-    #print(k)
-    haslo += k
-
-line = haslo + '                 ' + target + '\n'
-
-#print(desktop)
-print(sciezka)
-print(line)
-file = open(sciezka, 'a+')
-file.write(line)
+while always_true_until_false:
+    opcja = input("Wybierz opcję: Dodanie hasła (1), albo odczytanie hasła (2), albo zakończ działanie programu (3): ")
+    opcja_int = int(opcja)
+    sprawdz_typ(opcja_int)
+    if opcja_int == 1:
+        opt1_true = True
+        while opt1_true:
+            generowanie_hasla()  
+            dane = pd.read_csv(sciezka_csv, names=['Domena', 'Hasła '])
+            print(dane)
+            opt1_val = input("Chcesz wyjść z opcji nr 1? (T/N): ")
+            if opt1_val == 'T' or opt1_val == 't':
+                opt1_true = False
+    elif opcja_int == 2:
+        opt2_true = True
+        while opt2_true:
+            domena = str.upper(input("Do jakiej domeny poszukujesz hasła? "))
+            reader = csv.reader(open(sciezka_csv, 'r'))
+            slownik = {}
+            for row in reader:
+                k, v = row
+                slownik[k] = v
+            if domena in slownik:
+                print('Hasło dla: ' + domena + " to: " + slownik[domena])
+            else:
+                print("Nie ma hasła dla takiej domeny")
+            opt2_val = input("Chcesz wyjść z opcji nr 1? (T/N): ")
+            if opt2_val == 'T' or opt2_val == 't':
+                opt2_true = False
+    elif opcja_int == 3:
+        print('Wybrano zakończenie programu')
+        always_true_until_false = False       
+        sys.exit(0)
